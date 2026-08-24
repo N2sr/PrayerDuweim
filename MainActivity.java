5e7207f3881574803719f0942db031746e7b7e83
N2sr <nasrduaim@gmail.com>
2026-08-24 19:31:20 +0200

    النسخة الأولى

---
+++ MainActivity.java
@@ -0,0 +1,20 @@
+package com.duweim.prayerduweim;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.webkit.WebSettings;
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        WebView webView = new WebView(this);
+        webView.setWebViewClient(new WebViewClient());
+        WebSettings settings = webView.getSettings();
+        settings.setJavaScriptEnabled(true);
+        webView.loadUrl("file:///android_asset/index.html");
+        setContentView(webView);
+    }
+}
